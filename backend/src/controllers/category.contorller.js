9204a0dfaede56e9688c72b1da1988892b941dfd
Daniel <danieljgallina@gmail.com>
2023-03-16 13:22:20 -0600

    POST, DELETE and VIEW category

--- backend/src/controllers/category.contorller.js
+++ backend/src/controllers/category.contorller.js
@@ -17,3 +17,73 @@ export const getCategory = async (req, res) => {
       .json({ message: "Error getting request to backend D:" });
   }
 };
+
+// POST
+export const postCategory = async (req, res) => {
+  try {
+    const { category, description } = req.body;
+
+    const _category = await Category.create({
+      category,
+      description,
+    });
+
+    res.json({ message: "Category created" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Error getting request to backend D:" });
+  }
+};
+
+// DELETE
+export const deleteCategory = async (req, res) => {
+  try {
+    console.log(req.params);
+    const { id } = req.params;
+
+    const category = await Category.findOne({
+      where: {
+        CategoryID: id,
+        active: true,
+      },
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: "Error delete" });
+    }
+
+    await category.update({ active: false });
+
+    res.json({ message: "Category deleted" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Error getting request to backend D:" });
+  }
+};
+
+// VIEW
+export const viewCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findOne({
+      where: {
+        CategoryID: id,
+        active: true,
+      },
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    res.json(category);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error getting request to backend D:" });
+  }
+};
--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { getCategory } from "../controllers/category.contorller.js";
+import { getCategory, deleteCategory, postCategory, viewCategory } from "../controllers/category.contorller.js";
 
 const router = Router();
 
 // GET
 router.get("/category", getCategory);
+
+// POST
+router.post("/category/", postCategory);
+
+// DELETE
+router.delete("/category/:id", deleteCategory);
+
+// VIEW
+router.get("/category/:id", viewCategory);
 
 export default router;
