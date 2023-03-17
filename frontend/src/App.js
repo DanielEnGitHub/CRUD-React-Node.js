0636abd7d9c6d333f6fc2e1d75711c5ef47685fb
Daniel <danieljgallina@gmail.com>
2023-03-16 18:33:45 -0600

    Configuration Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Link, BrowserRouter } from "react-router-dom";
+import Rutas from "./routes";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <Link to="/"> Home</Link>
+        <Link to="products"> Products</Link>
+        <Link to="category"> Products</Link>
+        <Link to="reports"> Reports</Link>
       </header>
-    </div>
+      <Rutas/>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ frontend/src/Pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Welcom to La Ceiba S.A</div>
+  )
+}
+
+export default Home
