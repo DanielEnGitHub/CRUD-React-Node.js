eb90eb25437425ac61dea95ee8f4a7a5a214ed02
Daniel <danieljgallina@gmail.com>
2023-03-16 23:26:37 -0600

    Create category

--- frontend/src/Pages/category/Category.jsx
+++ frontend/src/Pages/category/Category.jsx
@@ -1,33 +1,52 @@
-import React from "react";
-import { getCategories, deleteCategories } from "../../conection/category";
+import React, { useState } from "react";
+import {
+  getCategories,
+  deleteCategories,
+  createCategory,
+} from "../../conection/category";
 import TableComponent from "../../components/Tables/Table";
 import useListAPI from "../../hooks/useListAPI";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
+// boostrap
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Form } from "react-bootstrap";
+
+import { useForm } from "react-hook-form";
 
 const Category = () => {
   const { data, getData } = useListAPI({ getFunction: getCategories });
 
+  // modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
   const swalDelete = (id) => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "You won't be able to revert this!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        deleteCategories(id)
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
+        deleteCategories(id);
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
         getData();
       }
-    })
-  }
+    });
+  };
 
   const columns = React.useMemo(
     () => [
@@ -62,8 +81,73 @@ const Category = () => {
     []
   );
 
+  const onSubmit = async (data) => {
+    await createCategory(data);
+    getData();
+  };
+
+  const form_input = [
+    {
+      name: "category",
+      type: "text",
+      placeholder: "Category name",
+      label: "Name",
+    },
+    {
+      name: "description",
+      type: "text",
+      placeholder: "Product description",
+      label: "Description",
+    },
+  ];
+
   return (
     <div style={{ padding: "30px" }}>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Category</Modal.Title>
+        </Modal.Header>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Modal.Body>
+            {/* form */}
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              {form_input.map((input) => {
+                return (
+                  <div key={input.name}>
+                    <Form.Label style={{ marginLeft: "10px" }}>
+                      {input.label}
+                    </Form.Label>
+                    <Form.Control
+                      style={{ marginBottom: "10px" }}
+                      type={input.type}
+                      placeholder={input.placeholder}
+                      name={input.name}
+                      {...register(input.name)}
+                    />
+                  </div>
+                );
+              })}
+            </Form.Group>
+            {/* errors will return when field validation fails  */}
+            {/* {errors.name && <span>This field is required</span>} */}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button type="submit" variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+      <Button
+        style={{ marginBottom: "2px" }}
+        onClick={() => handleShow()}
+        variant="primary"
+      >
+        Add Category
+      </Button>
       <TableComponent columns={columns} data={data} />
     </div>
   );
--- frontend/src/conection/category.js
+++ frontend/src/conection/category.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// list
 export const getCategories = async () => {
   try {
     const res = await axios.get("http://localhost:3000/api/category");
@@ -10,9 +11,24 @@ export const getCategories = async () => {
   }
 };
 
+// delete
 export const deleteCategories = async (id) => {
   try {
     const res = await axios.delete(`http://localhost:3000/api/category/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+// create
+export const createCategory = async (data) => {
+  try {
+    const res = await axios.post(
+      "http://localhost:3000/api/category",
+      data
+    );
     return res.data;
   } catch (error) {
     console.log(error);
